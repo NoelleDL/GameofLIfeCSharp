cec9f503552c480ef39aa825b8d4aff020f08c61
NoelleDL <noelle.legrain@gmail.com>
2018-07-08 18:21:40 +0100

    Test and method for any live cell with less than two neighbors dies

---
+++ GameofLifeKata/GameOfLife.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*Any dead cell with three dead neighbors comes to life.
+    Any live cell with less than two neighbors dies.
+    Any live cell with two to three neighbors continues to live.
+    Any live cell with more than three neighbors dies. */
+
+namespace GameofLifeKata
+{
+    public enum CellState
+    {
+        Alive,
+        Dead
+    }
+    public class GameOfLife
+    {
+        public static CellState GetNewState(CellState currentState, int liveNeighbors)
+        {
+            if (liveNeighbors < 2 && currentState == CellState.Alive)
+            {
+                currentState = CellState.Dead; 
+            }
+            return currentState; 
+        }
+    }
+}
---
+++ GameofLifeKataTests/GameOfLifeTests.cs
@@ -0,0 +1,23 @@
+using System;
+using GameofLifeKata;
+using Xunit;
+
+/*Any dead cell with three dead neighbors comes to life.
+    Any live cell with less than two neighbors dies.
+    Any live cell with two to three neighbors continues to live.
+    Any live cell with more than three neighbors dies. */
+
+namespace GameofLifeKataTests
+{
+    public class GameOfLifeTests
+    {
+        [Fact]
+        public void LiveCellFewerThan2NeighborsDies()
+        {
+            var currentState = CellState.Alive;
+            var liveNeighbors = 1;
+            CellState newState = GameOfLife.GetNewState(currentState, liveNeighbors);
+            Assert.Equal(CellState.Dead, newState);
+        }
+    }
+}
